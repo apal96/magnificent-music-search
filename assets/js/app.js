a0769f81cf0e64db2281e4711d9eb614c339fd85
SL <samanthl795@gmail.com>
2021-07-07 20:01:44 -0700

    Add API call to get album info

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,12 +6,14 @@ var relatedArtistsEl = $("#related-artists");
 var artistImagesEl = $("#artist-images");
 var similarArtistsEl = $("#similar-artists");
 var artistTracksEl = $("#artist-tracks");
+var albumInfoEl = $("#album-info");
 
 // Create a URL to fetch data from LastFM
 // method is the value for the LastFM method paramerer
 // artist is the value for the LastFM artist parameter (can be undefined if it is not needed)
 // track is the value for the LastFM track parameter (can be undefined if it is not needed)
-function createLastFMURL(method, artist, track) {
+// album is the value for the LastFM album parameter (can be undefined if it is not needed)
+function createLastFMURL(method, artist, track, album) {
     var queryURL = lastFMURL + method;
     queryURL += "&api_key=" + lastFMToken;
 
@@ -21,6 +23,10 @@ function createLastFMURL(method, artist, track) {
 
     if (track) {
         queryURL += "&track=" + track;
+    }
+
+    if (album) {
+        queryURL += "&album=" + album;
     }
     queryURL += "&format=json";
     queryURL = encodeURI(queryURL);
@@ -87,11 +93,12 @@ function getArtistInformation(artist) {
             relatedArtistsEl.append(similarArtistEl);
         }
 
-        // Clear any previous imaged
+        // Clear any previous images
         artistImagesEl.empty();
-        for (var idx = 0; idx < data.artist.image.length; idx++) {
+        // Show the last image
+        if (data.artist.image.length) {
             var artistImageEl = $("<img>");
-            artistImageEl.attr("src", data.artist.image[idx]["#text"]);
+            artistImageEl.attr("src", data.artist.image[data.artist.image.length - 1]["#text"]);
             artistImageEl.attr("alt", "picture of " + artist);
             artistImagesEl.append(artistImageEl);
         }
@@ -122,9 +129,11 @@ function getSimilarArtists(artist) {
         for (var idx = 0; (idx < data.similarartists.artist.length) && (idx < 10); idx++) {
             var similarArtistToDisplayEl = $('<div>');
             similarArtistToDisplayEl.text(data.similarartists.artist[idx].name);
-            for (var idx2 = 0; idx2 < data.similarartists.artist[idx].image.length; idx2++) {
+
+            if (data.similarartists.artist[idx].image.length) {
+                // Show the last image
                 var artistImageEl = $("<img>");
-                artistImageEl.attr("src", data.similarartists.artist[idx].image[idx2]["#text"]);
+                artistImageEl.attr("src", data.similarartists.artist[idx].image[data.similarartists.artist[idx].image.length - 1]["#text"]);
                 artistImageEl.attr("alt", "picture of " + data.similarartists.artist[idx].name);
                 similarArtistToDisplayEl.append(artistImageEl);
             }
@@ -218,6 +227,92 @@ function getSimilarSongs(songName, songArtist) {
                 console.log("No similar tracks found for: " + songName);
             }
         }
+      }
+    ).catch(function (error) {
+        console.log(error);
+    });
+}
+
+// Get albums for the artist
+function getAlbumsForArtist(artist) {
+    if (!artist) {
+        // artist must be provided
+        return;
+    }
+    
+    fetch(createLastFMURL("artist.gettopalbums", artist, undefined)).then(function(response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response.statusText);
+        }
+    }
+    ).then(function (data) {
+        console.log(data);
+        // Clear any previously displayed information
+        albumInfoEl.empty();
+        // Show the top albums
+        // Limit the display to 10 top albums
+        for (var idx = 0; (idx < data.topalbums.album.length) && (idx < 10); idx++) {
+            var albumEl = $('<div>');
+            var albumElName = $('<div>');
+            albumElName.text(data.topalbums.album[idx].name);
+            // Div to fill in info for the album when it is received from the API 
+            var albumInfoFromAPIEl = $('<div>');
+            albumInfoFromAPIEl.attr("data-album-info-for", data.topalbums.album[idx].name);
+            albumEl.append(albumElName);
+            albumEl.append(albumInfoFromAPIEl);
+            albumInfoEl.append(albumEl);
+            // Query the API for the album information
+            getAlbumInformation(artist, data.topalbums.album[idx].name);
+        }
+      }
+    ).catch(function (error) {
+        console.log(error);
+    });
+}
+
+// Get the information for an album
+function getAlbumInformation(artist, album) {
+    if (!artist || !album) {
+        // artist and album must be provided
+        return;
+    }
+
+    fetch(createLastFMURL("album.getinfo", artist, undefined, album)).then(function(response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response.statusText);
+        }
+    }
+    ).then(function (data) {
+        console.log(data);
+        // Escape ' and ] in the songName so they are not interpreted as part of the CSS selector
+        var escapedAlbumName = album.replace(/'/g, "\\'");
+        escapedAlbumName = escapedAlbumName.replace(/]/g, "\\]");
+        var parentElem = $("[data-album-info-for='" + escapedAlbumName + "']");
+        // Check that the summary is available
+        if (data.album && data.album.wiki) {
+            // Add a summary
+            var summaryText = data.album.wiki.summary;
+            var summaryEl = $('<div>');
+            summaryEl.html(summaryText);
+            parentElem.append(summaryEl);
+        } else {
+            console.log("No summary available for " + album);
+        }
+        // Add any images
+        var albumImagesEl = $('<div>');
+        if (data.album.image.length) {
+            // Show the last image
+            var albumImageEl = $("<img>");
+            albumImageEl.attr("src", data.album.image[data.album.image.length - 1]["#text"]);
+            albumImageEl.attr("alt",  album);
+            albumImagesEl.append(albumImageEl);
+        }
+        parentElem.append(albumImagesEl);
+
       }
     ).catch(function (error) {
         console.log(error);
